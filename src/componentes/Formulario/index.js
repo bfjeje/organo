31e4bdc005fb7963b9eba7c940d290b3afa93c25
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-04-11 11:40:08 -0300

    Aula 2 - Video 3 - Criando o botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
 
@@ -23,6 +24,9 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label="Time" itens={teams}/>
+                <Botao>
+                    🎆 Criar card
+                </Botao>
             </form>
         </section>
     )

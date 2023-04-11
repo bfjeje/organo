a68bcc5aca6d4c2f5808736fda30294c4a60e99e
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-04-11 12:02:11 -0300

    Aula 3 - Video 1 - Manipulando o input do usuario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,11 +3,15 @@ import "./CampoTexto.css"
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
-
+    let valor = 'Bruno Ellerbach'
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log()
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }

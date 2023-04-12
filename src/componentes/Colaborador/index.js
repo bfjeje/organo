d37b186cebe6d56e53b8cb28d900682e74254e13
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-04-12 17:56:15 -0300

    Aula 4 - Video 4 - Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) =>{
+    return(
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://www.github.com/bfjeje.png" alt="Foto de Bruno"/>
+            </div>
+            <div className="rodape">
+                <h4>Bruno Ellerbach</h4>
+                <h5>Instrutor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,4 +1,5 @@
 import './Time.css'
+import Colaborador from "../Colaborador";
 
 const Time = (props) => {
 
@@ -7,6 +8,8 @@ const Time = (props) => {
     return (
         <section className="time" style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
         </section>
     )
 }

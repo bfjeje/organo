ef8fa11eff05a7a75e906cd0f895750f131e8cc4
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-04-13 10:54:47 -0300

    Aula 4 - Video 6 - Escondiendo times vazios

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,6 +6,9 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria}
 
     return (
+        //Se for true, vai devolver a section. Renderização condicional.
+        // Tambem pode ser usado o operador ternario x ? y : z
+        props.colaboradores.length > 0 &&
         <section className="time" style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className="colaboradores">
